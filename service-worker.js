4566086845eb7ec3aa3e63bc9ec6a749da924f8d
Manpreet Singh Sidhu <preetsidhu2549@gmail.com>
2024-11-19 22:38:16 +0530

    Done

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,43 @@
-// const CACHE_NAME = "tic-tac-toe-cache-v1";
-// const FILES_TO_CACHE = [
-//   "/",
-//   "/index.html",
-//   "/styles.css",
-//   "/script.js",
-//   "/tic-tac-toe.png"
-// ];
+const CACHE_NAME = "tic-tac-toe-cache-v2"; // Increment version when updating files
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/tic-tac-toe.png"
+];
 
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache.addAll(FILES_TO_CACHE);
-//     })
-//   );
-// });
+// Install event: Cache new files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching files...");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
 
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((cachedResponse) => {
-//       return cachedResponse || fetch(event.request);
-//     })
-//   );
-// });
+// Fetch event: Serve from cache or fetch from network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cache) => {
-//           if (cache !== CACHE_NAME) {
-//             return caches.delete(cache);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
+// Activate event: Clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Deleting old cache:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
